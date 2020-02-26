e831469d3a377f79c275ec360c9133faa9b00df7
Xi Chen <stephaniechen319@gmail.com>
2020-02-26 19:48:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,5 +4,6 @@ def position_taken?(board, index)
   if board[index.to_i] == " " || board[index.to_i] == "" || board[index.to_i] == nil
     false
   elsif board[index.to_i] == "X" || board[index.to_i] == "O"
+    true
   end
 end
