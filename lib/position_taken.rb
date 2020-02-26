df17f32dfa87cb339fb46090552607bd36013ff5
Xi Chen <stephaniechen319@gmail.com>
2020-02-26 19:45:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def position_taken?(board, index)
+  if board[index.to_i] == "X" || "O"
+    true
+  end
+end
